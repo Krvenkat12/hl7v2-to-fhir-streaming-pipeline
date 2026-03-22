f5b777d0a589bb76059e31cf163101299ab9ee58
krvenkat12 <krvenkat@utexas.edu>
2026-03-21 21:57:40 -0400

    Added stop.bat to stop services

---
+++ stop.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ===================================================
+echo   Shutting down program...
+echo ===================================================
+
+echo Stopping and removing Docker containers...
+docker-compose down
+
+echo ===================================================
+echo   Successfully shut down!
+echo ===================================================
+pause
